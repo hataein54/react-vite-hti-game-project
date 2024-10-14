65caf60d2081c5e53255280dc921cf5741a5fa0b
WIN-TE9T2I6AN4G\ICT02_08 <hahataehaha@gmail.com>
2024-10-14 09:38:01 +0900

    폴더 생성

---
+++ src/components/rsp/RspCard.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./../App";
+import Footer from "../components/layout/Footer";
+import LottoBall from "../components/lotto/LottoBall";
+import LottoPage from "../components/pages/LottoPage";
+
+
+const RspCard = () => {
+  return
+}
+
+
+    
+
+
+export default RspCard;
