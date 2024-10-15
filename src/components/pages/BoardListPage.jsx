40c063efcfc7f48922e852a840f627a48abee640
WIN-TE9T2I6AN4G\ICT02_08 <hahataehaha@gmail.com>
2024-10-15 10:38:59 +0900

    게싶ㄴ 받아오는거 까지

--- src/components/pages/BoardListPage.jsx
+++ src/components/pages/BoardListPage.jsx
@@ -1,29 +1,38 @@
-
-import React, { useEffect, useState }  from "react";
-import boardService from "../../service/BoardService";
+import React, { useEffect, useState } from "react";
+import boardService from "../../services/BoardService";
 
 const BoardListPage = () => {
+  const [boards, setBoards] = useState([]);
 
-  const [boards, setBoards]= useState([]);
+  // 정리하면 아래와 같다.
 
-  useEffect(()=>{
-    console.log("use Effectiv 실행");
+  // useEffect(() => {
+  //   // 매 렌더링마다 실행
+  // });
+
+  // useEffect(() => {
+  //   // 컴포넌트가 처음 렌더링된 실행
+  // }, []);
+
+  // useEffect(() => {
+  //   // 컴포넌트가 처음 렌더링된 이후 실행
+  //   // a나 b가 변경되어 컴포넌트가 재렌더링된 이후 실행
+  // }, [a, b]);
+
+  useEffect(() => {
+    console.log("use Effective 실행");
     initBoards();
-  },[]);
+  }, []);
 
-  const initBoards= ()=> {
-    boardService.getPageingList()
-      .then((response)=>{
-        console.log(response);
-      })
-
+  const initBoards = () => {
+    boardService.getPagingList()
+    .then((response) => {
+      console.log(response);
+    });
   };
 
-  return ( 
-  <div>
-
-  </div>
-  );
-};
+  return(
+  <div>게시판 실행</div>
+)};
 
 export default BoardListPage;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,97 +1,48 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./../App";
+import LottoPage from "../components/pages/LottoPage";
+import RspPage from "../components/pages/RspPage";
+import BoardService from "../service/BoardService";
 
-import React,{useState} from 'react';
-import RspCard from '../components/rsp/RspCard';
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    loader: () => "가위바위보",
+    children: [
+      {
+        path: "/",
+        loader: () => "가위바위보",
+        element: <RspPage />,
+      },
+    ],
+  },
+  {
+    path: "/lotto",
+    element: <App />,
+    loader: () => "로또",
+    children: [
+      {
+        path: "/lotto",
+        loader: () => "로또",
+        element: <LottoPage />,
+      },
+    ],
+  },
+  {
+    path: "/boards",
+    element: <App />,
+    loader: () => "게시판",
+    children: [
+      {
+        path: "/boards",
+        loader: () => "게시판",
+        element: <BoardService />,
+      },
+    ],
+  },
+];
 
-import rock from '../assets/rsp/rock.jpg'
-import paper from '../assets/rsp/paper.jpg'
-import scissor from '../assets/rsp/scissor.jpg'
+const router = createBrowserRouter(routes);
 
-
-function RspPage() {
-
-    const [players , setPlayers] = useState([
-        {
-            id: 1,
-            username: '당신',
-            arrRsp :['가위','바위','보'],
-            img:'https://taegon.kim/wp-content/uploads/2018/05/image-5.png'
-        },
-        {
-            id: 2,
-            username: '심판',
-            arrRsp :[],
-            img:'https://taegon.kim/wp-content/uploads/2018/05/image-5.png'
-        },
-        {
-            id: 3,
-            username: '컴퓨터',
-            arrRsp :['랜덤생성'],
-            img:'https://taegon.kim/wp-content/uploads/2018/05/image-5.png'
-        }      
-    ])
-
-    const resultString = (com_rsp, your_rsp) => {
-        let result = '비겼습니다';
-      
-        if (com_rsp === your_rsp) {
-          return (result = '비겼습니다');
-        }
-      
-        if (com_rsp === 0) {
-          if (your_rsp === 1) result = '당신이 이겼습니다.';      
-          if (your_rsp === 2) result = '당신이 졌습니다.';
-        }
-      
-        if (com_rsp === 1) {
-          if (your_rsp === 0) result = '당신이 졌습니다.';      
-          if (your_rsp === 2) result = '당신이 이겼습니다.';
-        }
-      
-        if (com_rsp === 2) {
-          if (your_rsp === 1) result = '당신이 졌습니다.';      
-          if (your_rsp === 2) result = '당신이 이겼습니다.';
-        }
-      
-        return result;
-      };
-
-    const handleClick = e => { // eslint-disable-next-line
-        console.log(e.target.innerText)
-        
-        const rspArr = ['가위','바위','보']
-        const imgArr = [scissor,rock,paper]
-
-        let user_rsp = rspArr.indexOf(e.target.innerText)
-        let com_rsp = Math.floor(Math.random() * 2);
-
-        let result = resultString(com_rsp,user_rsp)
-
-        let copyPlayers = [...players]
-        //copyPlayers[1].arrRsp = result
-        copyPlayers[1].arrRsp = [result]
-        copyPlayers[2].arrRsp = [rspArr[com_rsp]]
-
-        copyPlayers[0].img = imgArr[user_rsp]
-        copyPlayers[2].img = imgArr[com_rsp]
-        
-        
-        setPlayers(copyPlayers)
-
-    };
-
-    return (
-        <main>
-            <div class="container mt-5">
-                <div class="row">
-                    {
-                        players && players.map( player => (
-                            <RspCard player={player} onClick={handleClick}></RspCard>
-                        ))
-                    }                    
-                </div>
-            </div>
-        </main>
-    );
-}
-
-export default RspPage;
+export { router, routes };
